c377e043e11408512c074800c823d57d10d67aff
Rffan <admin@rffan.com>
2015-12-01 17:04:04 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+__author__ = 'Administrator'
+wprpcurl='http://ailab.cc:81/xmlrpc.php'  #将里面的ailab.cc:81 改成你的WP的URL
+username = 'yourusername' #将账号改成你的WP的账号
+password = 'yourpasswd' #成你的WP密码
